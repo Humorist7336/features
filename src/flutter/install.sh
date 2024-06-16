d9e6cbf689d54b015102b6e3fc22677a16da197c
AlexPlay1 <42866177+AlexPlay1@users.noreply.github.com>
2024-06-16 14:12:28 +0000

    add flutter feature

---
+++ src/flutter/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update -y && apt-get upgrade -y;
+apt-get install -y curl git unzip xz-utils zip libglu1-mesa --no-install-recommends
+
+baseURL="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux"
+fileName="flutter_linux_3.22.2-stable.tar.xz"
+curl -LO "$baseURL/$fileName"
+tar -xf $fileName -C /usr/bin/
+rm $fileName
+
+chmod 755 /usr/bin/flutter
+chmod 755 /usr/bin/flutter/bin
